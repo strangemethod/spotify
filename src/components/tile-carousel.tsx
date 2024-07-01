965fe4f2c2c14308cfce9ee480e571cb6c325742
Andrew Bowles <>
2024-07-01 12:27:31 -0700

    react-spring animations

--- src/components/tile-carousel.tsx
+++ src/components/tile-carousel.tsx
@@ -1,41 +1,10 @@
-import { useSpringCarousel } from 'react-spring-carousel'
 import Tile from './tile.tsx'
 import '../styles/tile-carousel.scss'
 
 
 export default function TileCarousel({tiles}) {
-  // const slideGroups = [];
-  // const groupSize = 5;
-
-  // for (let i = 0; i < tiles.length; i += groupSize) {
-  //     const group = tiles.slice(i, i + groupSize);
-  //     slideGroups.push(group);
-  // }
-
-  // const slides = slideGroups.map((group, idx) => {
-  //   return {
-  //     id: `slide-${idx}`,
-  //     renderItem: (
-  //       group.map((tile, idx) => {
-  //         return (
-  //           <Tile key={idx}
-  //             title={tile.name || tile.album.name}
-  //             subtitle={tile.artists ? tile.artists[0].name : tile.album.artists[0].name}
-  //             image={tile.album.images[0].url}
-  //             style="small" />
-  //         )
-  //       })
-  //     )
-  //   }
-  // });
-
-  // const { carouselFragment } = useSpringCarousel({
-  //   items: slides
-  // })
-
   return (
      <div className="tile-carousel">
-        {/*{carouselFragment}*/}
         {tiles.map((tile, idx) => (
           <Tile key={idx}
             title={tile.name || tile.album.name}
--- src/components/tile-grid.tsx
+++ src/components/tile-grid.tsx
@@ -1,7 +1,32 @@
+import React, {useState} from 'react'
+import { useTrail, a, easings } from '@react-spring/web'
 import Tile from './tile.tsx'
 import '../styles/tile-grid.scss'
 
+
+const Trail: React.FC<{ open: boolean }> = ({ open, children }) => {
+  const items = React.Children.toArray(children)
+  console.log(easings)
+  const trail = useTrail(items.length, {
+    config: { mass: 5, tension: 3000, friction: 200, duration: 100, easing: easings.easeOutCubic},
+    opacity: open ? 1 : 0,
+    y: open ? 0 : -15,
+    from: { opacity: 0, y: -15},
+  })
+  return (
+    <>
+      {trail.map(({ height, ...style }, index) => (
+        <a.div key={index} className="child" style={style}>
+          {items[index]}
+        </a.div>
+      ))}
+    </>
+  )
+}
+
+
 export default function TileGrid({tiles}) {
+  const [open, set] = useState(true)
 
   const getImage = (tile) => {
     if (tile.images) {
@@ -29,15 +54,28 @@ export default function TileGrid({tiles}) {
 
   return (
     <div className="tile-grid">
-      {tiles && tiles.map((tile, idx) => ( 
+      <Trail open={open}>
+        {tiles && tiles.map((tile, idx) => ( 
           <Tile key={idx}
             image={getImage(tile)}
             subtitle={tile.subtitle || tile.description}
             style={tile.style}
             title={getTitle(tile)}
           />
-        ))
-      }
+        ))}
+      </Trail>
     </div>
+
+    // <div className="tile-grid">
+    //   {tiles && tiles.map((tile, idx) => ( 
+    //       <Tile key={idx}
+    //         image={getImage(tile)}
+    //         subtitle={tile.subtitle || tile.description}
+    //         style={tile.style}
+    //         title={getTitle(tile)}
+    //       />
+    //     ))
+    //   }
+    // </div>
   );
 }
