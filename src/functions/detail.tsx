eae43ca5835877daacaf595c85663a957e27c833
Andrew Bowles <>
2024-07-02 18:45:11 -0700

    detail pages

--- src/functions/detail.tsx
+++ src/functions/detail.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import ChipGrid from '../components/chip-grid.tsx'
 import GetColor from './get-color.tsx'
 import '../styles/detail.scss'
 
 export default function Detail(props) {
+  const [exanded, setExpanded] = useState(true)
+
+
   const cleanHtml = () => {
     // Strip invisibile characters
     let dom = document.createElement('div')
@@ -39,15 +43,15 @@ export default function Detail(props) {
         </div>
       </div>
       <main className="bg-gradient">
-        {props.detail.description &&
-          <div className="detail-description type-small"
-              dangerouslySetInnerHTML={{ __html: cleanHtml(props.detail.description)}}>
-          </div>
-        }
         {props.tracks &&
           <div className="detailTracks">
             <h2>Top Tracks</h2>
               <ChipGrid {...props} chips={props.tracks} max="9" />
+          </div>
+        }
+        {props.detail.description &&
+          <div className="detail-description type-small"
+              dangerouslySetInnerHTML={{ __html: cleanHtml(props.detail.description)}}>
           </div>
         }
       </main>
